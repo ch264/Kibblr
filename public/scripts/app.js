c0117db0b0fcb6729b14d42c09276f981217af29
brandonmcastillo <brandonmaerina@gmail.com>
2019-02-18 12:49:56 -0800

    Updated HTML

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,32 +1,51 @@
+//jshint esversion:6
 $(function() {
-    console.log( "ready!" );
+    console.log("ready!");
 
-//Click on header leads to main page
-$('.navbar-brand').click(function(){
-  window.location.href = "/Users/User/Desktop/Project01/views/index.html";
-})
-//Click on review button leads to page
-$('.review').click(function(){
-  window.location.href = "/Users/User/Desktop/Project01/views/reviewForm.html";
- 
-})
-//Sign Up Form Validation
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
+    // AJAX Calls
+    const handleResponse = (json) => {
+        console.log(json);
+    };
+
+    const handleError = (err) => {
+        console.log("error");
+    };
+
+    //Create a user through signup
+    $.ajax({
+        method: 'POST',
+        url: '/api/user',
+        success: handleResponse,
+        error: handleError
     });
-  }, false);
-})();
+
+    //Sign a user in 
+    $.ajax({
+        method: 'GET',
+        url: '/api/user',
+        success: handleResponse,
+        error: handleError
+    });
+
+    //Enter a review
+    // $.ajax({
+    //   method:
+    //   url:
+    //   success:
+    //   error:
+    // })
+
+
+    // Redirection through clicks
+    //Clicking on header leads to main page
+    $('.navbar-brand').click(function() {
+        window.location.href = "/Users/User/Desktop/Project01/views/index.html";
+    });
+    //Clicking on review button leads to review page
+    $('.review').click(function() {
+        window.location.href = "/Users/User/Desktop/Project01/views/reviewForm.html";
+    });
+
+
 
 });
