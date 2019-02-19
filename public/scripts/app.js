5c1b23cee10dd65883c059e7945ef0ef42ad661d
brandonmcastillo <brandonmaerina@gmail.com>
2019-02-18 20:35:18 -0800

    Updated href CSS and Script src for all HTML documents

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,25 +3,27 @@ $(function() {
     console.log("ready!");
 
     // AJAX Calls
-    const handleResponse = (json) => {
-        console.log(json);
-    };
+    // const handleResponse = (json) => {
+    //     console.log(json);
+    // };
 
-    const handleError = (err) => {
-        console.log("error");
-    };
+    // const handleError = (err) => {
+    //     console.log("error");
+    // };
 
 
 
 
     // Redirection through clicks
     //Clicking on header leads to main page
-    $('.navbar-brand').click(function() {
-        window.location.href = "/Users/User/Desktop/Project01/views/index.html";
+    $('.navbar-brand').click(function(e) {
+        e.preventDefault();
+        window.location.href = "/";
     });
     //Clicking on review button leads to review page
-    $('.review').click(function() {
-        window.location.href = "/Users/User/Desktop/Project01/views/reviewForm.html";
+    $('.review').click(function(e) {
+        e.preventDefault();
+        window.location.href = "http://localhost:3000/reviewForm.html";
     });
 
 
